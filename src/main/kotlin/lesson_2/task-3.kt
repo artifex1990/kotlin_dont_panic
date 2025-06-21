107dfdca3f6edbbb4e1d58be2632819125578be7
pavel demyanov <pavel@demjanov.ru>
2025-06-22 00:21:17 +0300

    Урок 2: Задача 3 Что сделано:
    Сайт с расписанием поездов получает данные с сервера. Сервер посылает время выезда и время в пути, а время прибытия вычисляется из них. Сервер прислал данные, что поезд выехал в 9:39 и будет в пути 457 минут.
    
    - [X] Создай целочисленные переменные и проинициализируй их этими данными;
    - [X] Напиши программу, которая обработает данные и подсчитает час и минуту прибытия поезда;
    - [X] Выведи результат в консоль.

---
+++ src/main/kotlin/lesson_2/task-3.kt
@@ -0,0 +1,15 @@
+package lesson_2
+
+const val MINUTES_ON_HOUR = 60
+const val SECONDS_ON_MINUTE = 60
+
+fun main() {
+    val hourDeparture = 9
+    val minuteDeparture = 39
+    val travelTimeMinute = 457
+    val commonTravelMinute = hourDeparture * MINUTES_ON_HOUR + minuteDeparture + travelTimeMinute
+    val hourArrival = commonTravelMinute / SECONDS_ON_MINUTE
+    val minuteArrival = commonTravelMinute % SECONDS_ON_MINUTE
+
+    println("Поезд прибудет в %02d:%02d".format(hourArrival, minuteArrival))
+}

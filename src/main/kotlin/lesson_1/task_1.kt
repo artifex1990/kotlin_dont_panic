73b8d4f568d6f7d35b5f0e1773a9c3533b19fb30
pavel demyanov <pavel@demjanov.ru>
2025-06-21 18:33:04 +0300

    Урок 1: Задача 4 Что сделано:
    Записал в переменные соответствующие значения, явно указывая их тип. Для всех переменных выбирал тип данных, занимающий наименьшее возможное место в памяти, при этом вмещающий нужное значение (например для числа 0 выбирай тип Byte и так далее).
    
    Список переменных:
    - [X] length –расстояние, которое пролетел Гагарин пока был в космосе в миллиметрах;
    - [X] age – возраст Гагарина на момент полета (целых лет);
    - [X] partOfDay – какую часть дня Гагарин провел в космосе;
    - [X] seconds – количество секунд которое Гагарин провел в космосе;
    - [X] partOfYear – какую часть года Гагарин провел в космосе;
    - [X] apogee – апогей орбиты Гагарина в метрах.
    
    И вывел все эти переменные в столбик (каждая с новой строки).
    
    Данные для задачи:
    Расстояние 40_868_600_000
    Возраст – 27
    Часть дня – 0.075
    Секунды – 6480
    Часть года – 2.0547945205479453E-4
    Апогей орбиты – 327000

---
+++ src/main/kotlin/lesson_1/task_1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_1
+
+fun main() {
+    var ordersPerDay: Long = 75
+    val gratitudeForPurchase: String = "Спасибо за покупку!"
+
+    ordersPerDay += 1
+
+    println("Количество покупок за день: $ordersPerDay")
+    println(gratitudeForPurchase)
+}

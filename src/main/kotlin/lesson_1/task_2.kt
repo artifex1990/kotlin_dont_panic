3bd78c7ec2369c32725cd07ef8a58252944b64e2
pavel demyanov <pavel@demjanov.ru>
2025-06-21 18:33:04 +0300

    Урок 1: Задача 3 Что сделано:
    Объявил переменные и вывел данные о полете в космос первого человека.
    
    - [X] Запиши в новую переменную year год полета Юрия Гагарина в космос;
    - [X] Объяви и проинициализируй переменные hour и minute – час и минуту взлета соответственно;
    - [X] Выведи год, час и минуту взлета по одной переменной на каждую строку;
    - [X] Затем измени значения переменных hour и minute, чтобы они теперь имели значение часа и минуты посадки;
    - [X] Выведи время посадки в одной строке в формате [часы:минуты]. Пример: 10:55.
    
    - [X] Определил, где в программе переменные должны задаваться через val, а где через var.
    
    Данные для задачи:
    Год полета – 1961
    Взлет – 09:07
    Посадка – 10:55

---
+++ src/main/kotlin/lesson_1/task_2.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_1
+
+fun main() {
+    var ordersPerDay: Long = 75
+    var staffNumber: Int = 2000
+    val staffLessYesterday = 6
+    val gratitudeForPurchase: String = "Спасибо за покупку!"
+
+    ordersPerDay += 1
+
+    println("Количество покупок за день: $ordersPerDay")
+    println(gratitudeForPurchase)
+
+    println("Количество сотрудников работающих в интернет магазине: $staffNumber человек")
+
+    println("Вчера от нас ушло $staffLessYesterday человек!")
+    staffNumber -= staffLessYesterday
+
+    println("Сейчас работает: $staffNumber человек")
+}

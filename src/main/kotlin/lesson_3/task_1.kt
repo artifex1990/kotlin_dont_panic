54cdc49dfb6a32b8e1ae6d66e25d432500e90889
pavel demyanov <pavel@demjanov.ru>
2025-07-10 01:28:56 +0300

    KS-3-1-greetings-time-day: Функционал, который выводит приветствие с именем пользователя на экран (в нашем случае в консоль). Tекст приветствия может меняться в зависимости от времени суток. Так, например, выводится приветствие при входе в приложение Сбербанка. Что сделано:
    Функционал, который выводит приветствие с именем пользователя на экран (в нашем случае в консоль). Tекст приветствия может меняться в зависимости от времени суток. Так, например, выводится приветствие при входе в приложение Сбербанка.
    
    - [X] Приветствие и имя пользователя должны храниться в отдельных переменных. Вывести в консоль два приветствия (для дня и для вечера), перезаписывая данные в одну исходную переменную.

---
+++ src/main/kotlin/lesson_3/task_1.kt
@@ -0,0 +1,12 @@
+package lesson_3
+
+fun main() {
+    val userName = "Павел"
+    var greetings = "Добрый день"
+
+    println("$greetings $userName")
+
+    greetings = "Добрый вечер"
+
+    println("$greetings $userName")
+}

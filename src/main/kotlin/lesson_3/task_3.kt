5f1e1e3c685df2ac3a12f39cf3e15fc5bc895e41
pavel demyanov <pavel@demjanov.ru>
2025-07-10 01:28:56 +0300

    KS-3-3-multiples-table: Приложение выводящее таблицу умножения для заданного числа.
    Приложение выводящее таблицу умножения для заданного числа.
    
    Пример вывода:
    6 x 1 = 6
    6 x 2 = 12
    …
    6 x 9 = 54
    
    Что сделано:
    
    – [X] Вместо числа 6 использовать данные из объявленной переменной;
    – [X] Вычислять значения прямо в строке;
    – [X] Вывод всей таблицы нужно сделать в одной функции println(), которая вызывается один раз.

---
+++ src/main/kotlin/lesson_3/task_3.kt
@@ -0,0 +1,19 @@
+package lesson_3
+
+fun main() {
+    val n = 8
+
+    println(
+        """
+        |$n x 1 = ${n * 1}
+        |$n x 2 = ${n * 2}
+        |$n x 3 = ${n * 3}
+        |$n x 4 = ${n * 4}
+        |$n x 5 = ${n * 5}
+        |$n x 6 = ${n * 6}
+        |$n x 7 = ${n * 7}
+        |$n x 8 = ${n * 8}
+        |$n x 9 = ${n * 9}
+    """.trimMargin()
+    )
+}

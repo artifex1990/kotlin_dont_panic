55c3f433501101a7ffd850c7c2462f2e97f50d5e
pavel demyanov <pavel@demjanov.ru>
2025-07-11 00:06:20 +0300

    KS-2-5-app-calculate-contribution: фикс для программы калькулятора.
    Вынес в константу приведение к процентам.
    Отделил расчет процентов от объявлении переменных.

--- src/main/kotlin/lesson_2/task_5.kt
+++ src/main/kotlin/lesson_2/task_5.kt
@@ -2,11 +2,14 @@ package lesson_2
 
 import kotlin.math.pow
 
+const val PERCENT_BASE = 100
+
 fun main() {
     val clientContribution = 70000.0
-    val interestRate = 0.167
+    val interestRate = 16.7
     val depositTermYear = 20
-    val compoundInterestFormula = clientContribution * (1 + interestRate).pow(depositTermYear)
+
+    val compoundInterestFormula = clientContribution * (1 + interestRate / PERCENT_BASE).pow(depositTermYear)
 
     println("%.03f".format(compoundInterestFormula))
 }

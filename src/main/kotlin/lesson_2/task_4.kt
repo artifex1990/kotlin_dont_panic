2615bf51a3dc01a16ec4d9f94ae3500277e30f4d
pavel demyanov <pavel@demjanov.ru>
2025-07-11 00:02:00 +0300

    KS-2-4-crystall-buff: фикс для вычисления баффа.
    Вынес в константу приведение к процентам.
    Сделал как в условии проценты, в виде целого числа.

--- src/main/kotlin/lesson_2/task_4.kt
+++ src/main/kotlin/lesson_2/task_4.kt
@@ -1,12 +1,14 @@
 package lesson_2
 
+const val PERCENT_BASE = 100
+
 fun main() {
-    val buf = 0.2
+    val buffPercent = 20
     val crystal = 7
     val ironStone = 11
-    val bufCrystal = crystal * buf
-    val bufIronStone = ironStone * buf
+    val bufCrystal = crystal * buffPercent / PERCENT_BASE
+    val bufIronStone = ironStone * buffPercent / PERCENT_BASE
 
-    println("Дополнительное количество кристалов с баффом %.0f".format(bufCrystal))
-    println("Дополнительное количество железной руды с баффом %.0f".format(bufIronStone))
+    println("Дополнительное количество кристалов с баффом $bufCrystal")
+    println("Дополнительное количество железной руды с баффом $bufIronStone")
 }

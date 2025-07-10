9b3d540693dfff6bfad2a4b56491912de8929121
pavel demyanov <pavel@demjanov.ru>
2025-07-10 23:16:43 +0300

    KS-2-1-average: Задача на среднее арифметическое.
    Убрал литеральную константу, обозначающую количество студентов, перенес в отдельную переменную numberOfStudents.
    
    Отделил пустой строчкой формулу расчета среднего арифметического оценок студентов.

--- src/main/kotlin/lesson_2/task-1.kt
+++ src/main/kotlin/lesson_2/task-1.kt
@@ -5,7 +5,9 @@ fun main() {
     val scoresStudent2 = 4.0
     val scoresStudent3 = 3.0
     val scoresStudent4 = 5.0
-    val avg = (scoresStudent1 + scoresStudent2 + scoresStudent3 + scoresStudent4) / 4
+    val numberOfStudents = 4
+
+    val avg = (scoresStudent1 + scoresStudent2 + scoresStudent3 + scoresStudent4) / numberOfStudents
 
     println("%.02f".format(avg))
 }

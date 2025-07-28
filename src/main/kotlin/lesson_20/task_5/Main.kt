a2c5c39625343df43f9c69ec87b2c3994e08a076
pavel demyanov <pavel@demjanov.ru>
2025-07-28 16:46:08 +0300

    fix commits for KS-20-4 и KS-20-5

--- src/main/kotlin/lesson_20/task_5/Main.kt
+++ src/main/kotlin/lesson_20/task_5/Main.kt
@@ -1,15 +1,34 @@
 package lesson_20.task_5
 
 fun main() {
-    val items = listOf("Кнопка", "Меню", "Слайдер", "Чекбокс", "Переключатель")
+    val robot = Robot()
 
-    val printActions = items.map { item ->
-        { println("Нажат элемент [$item]") }
+    val reverseWordsModifier: (String) -> String = { phrase ->
+        phrase.split(" ").reversed().joinToString(" ")
     }
 
-    printActions.forEachIndexed { index, action ->
-        if (index % 2 == 1) {
-            action()
-        }
+    robot.say()
+    robot.setModifier(reverseWordsModifier)
+    robot.say()
+}
+
+class Robot {
+    private val phrases = listOf(
+        "Я устал от программирования!",
+        "Мир тлен!",
+        "В слове п*здец - что-то есть!",
+        "Опять бэки завалили прод!",
+        "Фронты до сей поры думают, что они программисты!"
+    )
+
+    private var modifier: (String) -> String = { it }
+
+    fun setModifier(modifier: (String) -> String) {
+        this.modifier = modifier
+    }
+
+    fun say() {
+        val randomPhrase = phrases.random()
+        println(modifier(randomPhrase))
     }
 }
